71fba98e11a0619e13c4c6919cd106677eeabf39
uanela <uanelaluiswayne@gmail.com>
2026-01-14 13:29:11 +0200

    adding plugin folder

--- lua/nvim-hello-command/init.lua
+++ lua/nvim-hello-command/init.lua
@@ -1,12 +1,6 @@
-local M = {}
-
-function M.setup()
-  vim.api.nvim_create_user_command("Greet",
-    function(opts)
-      print("Hello from the other side " .. opts.args)
-    end,
-    { nargs = 1 }
-  )
-end
-
-return M
+vim.api.nvim_create_user_command("Greet",
+  function(opts)
+    print("Hello from the other side " .. opts.args)
+  end,
+  { nargs = 1 }
+)
