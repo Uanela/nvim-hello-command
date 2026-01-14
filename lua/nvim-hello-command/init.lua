8963ca33769c380bec850e80bd5cdea54470789c
uanela <uanelaluiswayne@gmail.com>
2026-01-14 12:50:45 +0200

    making it a module

--- lua/nvim-hello-command/init.lua
+++ lua/nvim-hello-command/init.lua
@@ -1,6 +1,12 @@
-vim.api.nvim_create_user_command("Greet",
-  function(opts)
-    print("Hello from the other side " .. opts.args)
-  end,
-  { nargs = 1 }
-)
+local M = {}
+
+function M.setup()
+  vim.api.nvim_create_user_command("Greet",
+    function(opts)
+      print("Hello from the other side " .. opts.args)
+    end,
+    { nargs = 1 }
+  )
+end
+
+return M
